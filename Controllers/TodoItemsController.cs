85d738ef6298334baf277bd4569b330ffc49cc3d
Ludwig Nojd <ludwig.nojd@hotmail.se>
2023-04-29 12:44:28 +0200

    first commit web api

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,109 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+        // GET: api/TodoItems search
+         [HttpGet("search")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems(string searchString)
+        {
+            var todoItems = from item in _context.TodoItems
+                            select item;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                todoItems = todoItems.Where(item => item.Name.Contains(searchString));
+            }
+
+            return await todoItems.ToListAsync();
+        }
+
+        [HttpGet("filter")]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItem(bool isComplete)
+        {
+            var todoItems = from item in _context.TodoItems
+                            where item.IsComplete == isComplete
+                            select item;
+
+            return await todoItems.ToListAsync();
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // POST: api/TodoItems
+       [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT: api/TodoItems/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
